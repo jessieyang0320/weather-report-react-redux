e15c5aba391ecdb4b5039810f86bfc7682cdcb1c
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-25 20:29:20 -0400

    install react-spartline for charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine, SparklinesBars } from 'react-sparklines';
+
 
 class WeatherList extends Component {
 
 
 
 	renderWeather(cityData){
-		
+
 		const name = cityData.city.name
+		const temps = cityData.list.map(weather => weather.main.temp)
+
 
 		return(
-			<tr key={name}>
-				<td>{name}</td>
-			</tr>
+			
+				<tr key={name}>
+					<td>{name}</td>
+					<td>
+						<Sparklines height = {120} width={180} data={temps}>
+							<SparklinesLine color = "red" />
+						</Sparklines>
+
+					</td>
+				</tr>
+		
 
 			)
 
